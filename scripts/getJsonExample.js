ae0ece955cc9cde4d551404a3c83df277f146098
devjanaprime <dev@primeacademy.io>
2016-05-21 22:56:26 -0500

    reset to ajaxBasic

--- scripts/getJsonExample.js
+++ scripts/getJsonExample.js
@@ -30,21 +30,4 @@ $( function() {
        }); // end ajax  object
   }); // end click getJSONAjax button
 
-  $('#getJSONPAjax' ).click( function(){
-    console.log( 'button clicked' );
-     $.ajax({
-       url: './test.json',
-       dataType: 'jsonp',
-       success: function( data ){
-          console.log( 'in ajax success' );
-          console.log( data );
-         }, // end success
-       statusCode: {
-          404: function(){
-             alert( 'error connecting to server' );
-          } // end 404
-         } // end statusCode
-       }); // end ajax  object
-  }); // end click getJSONAjax button
-
 });
